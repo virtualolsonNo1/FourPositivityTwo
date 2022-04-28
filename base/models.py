797b9c887585064e88ef930a6d5983007bd93ed5
TMozingo13 <TMozingo13@users.noreply.github.com>
2022-04-27 20:00:15 -0500

    finished profile page

--- base/models.py
+++ base/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.dispatch import receiver
 # Create your models here.
 
+
 class Message(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sender')
     receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='receiver')
